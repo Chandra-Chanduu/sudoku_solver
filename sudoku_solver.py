a6050992cda90ad83483490b0724ba46306df640
Chandra-Chanduu <54219015+Chandra-Chanduu@users.noreply.github.com>
2023-11-02 19:43:57 +0530

    Update sudoku_solver.py
    Added input to it

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,44 +5,61 @@ solving the sudoku using backtracking:
 -checking the numbers 1 to 9 in the empty places
 -if the number we inserted dis wrong then we backtrack and try another number
 -backtracking goes on till the last place to be filled and solves the board    '''''
-board=[]
-# Enter the input in a 9*9 matrix format and represent the numbers which has to be find with 0
 
-for i in range(9):
-    board.append([int(j) for j in input().split()])
 
-# print_board function helps to print the  solved sudoku
+# Sudoku Solver using Backtracking Algorithm
+
+# The input Sudoku board is represented as a 9x9 matrix.
+# Empty cells are indicated with 0.
+
+# Define the input Sudoku board
+board = [
+    [4, 7, 0, 1, 0, 0, 0, 0, 3],
+    [6, 0, 0, 5, 0, 3, 0, 0, 0],
+    [0, 0, 5, 0, 7, 9, 0, 2, 0],
+    [0, 1, 0, 7, 0, 6, 2, 0, 0],
+    [0, 5, 0, 0, 0, 8, 9, 4, 0],
+    [0, 0, 0, 0, 0, 0, 0, 3, 6],
+    [0, 0, 0, 0, 0, 0, 0, 8, 0],
+    [7, 0, 3, 0, 0, 5, 0, 0, 1],
+    [2, 4, 8, 0, 9, 0, 6, 0, 0]
+]
+
+# Function to print the Sudoku board
 def print_board(bo):
+    # Add horizontal lines to separate 3x3 subgrids
     for i in range(len(bo)):
         if i % 3 == 0:
             print("- " * 12)
         for j in range(len(bo[0])):
+            # Add vertical lines to separate 3x3 subgrids
             if j % 3 == 0:
                 print("| ", end="")
+            # Print the cell value
             if j == 8:
                 print(bo[i][j])
             else:
                 print(str(bo[i][j]) + " ", end="")
 
-# finding all the 0's i.e., the numbers which has to be filled
-def empty(bo):
+# Function to find the first empty cell (cell with value 0)
+def find_empty_cell(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return (i, j)
     return None
 
-#checking the number which we entered is valid  or not
-def valid(bo, num, pos):
-    # validating the number is not present in the row of the sudoku board
+# Function to check if a number is valid to place in a specific position
+def is_valid(bo, num, pos):
+    # Check if the number is not in the same row
     for i in range(len(bo)):
         if bo[pos[0]][i] == num and pos[1] != i:
             return False
-        # validating the number is not present in the column of the sudoku board
+    # Check if the number is not in the same column
     for i in range(len(bo)):
         if bo[i][pos[1]] == num and pos[0] != i:
             return False
-    # validating the number is not present in the box i.e., 3*3 where the range of 1 to 9 are present
+    # Check if the number is not in the same 3x3 box
     box_x = pos[1] // 3
     box_y = pos[0] // 3
     for i in range(box_y * 3, box_y * 3 + 3):
@@ -51,27 +68,24 @@ def valid(bo, num, pos):
                 return False
     return True
 
-#solving the sudoku using backtracking
-def solve(bo):
-    #reetriving the position of the empty places i.e., 0's
-    find = empty(bo)
-    #if no empty positions left then the board is solved and returns true
-    if not find:
+# Main function to solve the Sudoku puzzle using backtracking
+def solve_sudoku(bo):
+    empty_cell = find_empty_cell(bo)
+    # If no empty cells are found, the Sudoku is solved
+    if not empty_cell:
         return True
     else:
-        row, col = find
-    for i in range(1, 10):
-        #validating
-        if valid(bo, i, (row, col)):
-            #checking each number from 1 to 9
-            bo[row][col] = i
-
-            if solve(bo):
+        row, col = empty_cell
+    for num in range(1, 10):
+        if is_valid(bo, num, (row, col)):
+            bo[row][col] = num
+            if solve_sudoku(bo):
                 return True
             bo[row][col] = 0
     return False
 
-#calling the solve function and solves the board
-solve(board)
-#printing the solved sudoku board
+# Call the solve_sudoku function to solve the Sudoku board
+solve_sudoku(board)
+
+# Print the solved Sudoku board
 print_board(board)
